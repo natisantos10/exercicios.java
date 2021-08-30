9bc9c0c5df002763d7fc65e3688ef49d887e7ab0
Natália Santos <87249524+natisantos10@users.noreply.github.com>
2021-08-30 16:36:06 -0300

    Create SomadeNumeros.java

---
+++ SomadeNumeros.java
@@ -0,0 +1,13 @@
+package src;
+
+public class somaNumeros {
+    public static void main(String[] args){
+        var n1 = 8;
+        var n2 = 5;
+
+        var soma = (n1 + n2);
+
+        System.out.println("A soma entre 8 e 5 é igual a  " + soma);
+
+    }
+}
