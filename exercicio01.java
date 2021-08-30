981a15b4ff35c3186fad86e68ebb63d1e36a9de2
natisantos10 <nataliadesideriondm@gmail.com>
2021-08-29 21:34:12 -0300

    MediaSalarial

---
+++ exercicio01.java
@@ -0,0 +1,15 @@
+public class MediaSalario{
+    public static void main(String[] args){
+        double n1, n2, n3, n4, media;
+         
+        n1 = 3500.40;
+        n2 = 38500.80;
+        n3 = 4200.60;
+        n4 = 9800.75;
+
+        media = (n1 + n2 + n3 + n4) / 4;
+
+        System.out.println("A média Salarial é" + media)
+    }
+}
+
