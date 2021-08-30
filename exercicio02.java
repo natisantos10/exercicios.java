a41516d2c806b2adc2c01175a64cf961d3516ada
Natália Santos <87249524+natisantos10@users.noreply.github.com>
2021-08-29 22:01:03 -0300

    Add files via upload

---
+++ exercicio02.java
@@ -0,0 +1,14 @@
+public class Cavalos{
+    public static void main(String[] args){
+        double m, h, t, cavalos;
+
+        m = 3.4;
+        h = 2.6;
+        t = 4.8;
+
+        cavalos = ("m * h / t") /745,69;
+
+        System.out.println("A quantidade de cavalos necessaria é" + cavalos)
+
+    }
+}
