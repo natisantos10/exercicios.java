c27220ba7f4411fdf6a6914451767dc4accc8fb5
Natália Santos <87249524+natisantos10@users.noreply.github.com>
2021-08-30 21:25:33 -0300

    Create operadores.java

---
+++ operadores.java
@@ -0,0 +1,49 @@
+package src;
+
+public class MaiorQue {
+
+    public static void main(String[] args){
+
+        /*var a = 2;
+        var b = 4;
+
+        var c = 2>4;
+        System.out.println(c);*/ /*!-->FALSE--<
+        */
+
+
+        /*int a = 8;
+        int b = 16;
+
+        boolean c = (a>=b); !-->FALSE-->
+
+        System.out.println(c);*/
+
+        /*int a = 10;
+        int b = 40;
+
+        boolean c = (a<b); !-->TRUE-->
+
+        System.out.println(c);*/
+
+       /*var a = 36;
+        var b = 58;
+        var c = 60;
+
+        boolean d = ((a==b) && (c>a)); !-->FALSE-->
+
+        System.out.println(d);*/
+
+        var a = 90;
+        var b = 68;
+        var c = 42;
+
+        boolean d = ((a>b) || (c==b));
+
+        System.out.println(d); !-->TRUE-->
+
+
+
+
+    }
+}
