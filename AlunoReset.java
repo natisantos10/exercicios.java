c4ab3c4cb9fa6a75c65758e10b549e1676c0ce6d
Natália Santos <87249524+natisantos10@users.noreply.github.com>
2021-09-04 15:23:07 -0300

    Create AlunoReset.java

---
+++ AlunoReset.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.Scanner;
+
+public class AlunoReset {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Sou Aluno do Level 1 do CWI Reset!");
+    }
+}
